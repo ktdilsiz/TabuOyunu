a7d542deb1555d8e5f4b82f2f941e312765a3e9e
Kemal Dilsiz <ktdilsiz@gmail.com>
2017-07-07 16:03:32 -0500

    first commit

---
+++ app/src/main/java/com/ktdilsiz/tabuyasak/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ktdilsiz.tabuyasak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
